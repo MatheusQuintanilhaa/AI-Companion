89ff701d62fa24c03769c08ae60764068463830b
Matheus <matheussantos.quintanilha@gmail.com>
2023-08-22 00:12:05 -0300

    Companion Creation Form - I Finished this part

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,8 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { ClUploadButton } from 'next-cloudinary';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { CldUploadButton } from 'next-cloudinary';
+
+import { Button } from '@/components/ui/button';
+import { ImageIcon, X } from 'lucide-react';
 
 interface ImageUploadProps {
   value: string;
@@ -22,43 +25,42 @@ export const ImageUpload = ({
   }, []);
 
   if (!isMounted) {
-    return null;
+    return false;
   }
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-      <ClUploadButton
-        options={{
-          maxFiles: 1,
-        }}
+      <CldUploadButton
+        options={{ maxFiles: 1 }}
+        onUpload={(result: any) => onChange(result.info.secure_url)}
         uploadPreset="tbp3p9y3"
       >
         <div
           className="
-        p-4
-        border-4
-        border-dashed
-        border-primary/10
-        rounded-lg
-        hover:opacity-75
-        transition
-        flex
-        flex-col
-        space-y-2
-        items-center
-        justify-center
-        "
+            p-4
+            border-4
+            border-dashed
+            border-primary/10
+            rounded-lg
+            hover:opacity-75
+            transition
+            flex
+            flex-col
+            space-y-2
+            items-center
+            justify-center
+          "
         >
           <div className="relative h-40 w-40">
             <Image
               fill
               alt="Upload"
-              src="/placeholder.svg"
+              src={value || '/placeholder.svg'}
               className="rounded-lg object-cover"
             />
           </div>
         </div>
-      </ClUploadButton>
+      </CldUploadButton>
     </div>
   );
 };
